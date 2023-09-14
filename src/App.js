1ff15159c0a75e9946b353d4d640464f3df55b60
Moreno Dore <moreno.dore@tuta.io>
2023-09-14 17:38:03 +0200

    Add initial meme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
   const [userTopText, setUserTopText] = useState();
   const [userBottomText, setUserBottomText] = useState();
-  const [userMeme, setUserMeme] = useState();
-  const [userTemplate, setUserTemplate] = useState(
-    'https://api.memegen.link/images/older.png',
-  );
+  const [userTemplate, setUserTemplate] = useState('older');
 
   const templates = 'https://api.memegen.link/images/' + `${userTemplate}.png`;
-
-  // {
-  //   {"id":"older",
-  //   "name":"An Older Code Sir, But It Checks Out",
-  //   "lines":2,
-  //   "overlays":0,
-  //   "styles":[],
-  //   "blank":"https://api.memegen.link/images/older.png",
-  //   "example":{"text":["it's an older meme sir","but it checks out"],
-  //   "url":"https://api.memegen.link/images/older/it's_an_older_meme_sir/but_it_checks_out.png"},
-  //   "source":"http://knowyourmeme.com/memes/its-an-older-meme-but-it-checks-out","keywords":[],
-  //   "_self":"https://api.memegen.link/templates/older"}
-  // }
 
   return (
     <div>
@@ -48,14 +32,14 @@ export default function App() {
       <label>
         Meme template{' '}
         <input
-          value={userMeme}
+          value={userTemplate}
           onChange={(bottom) => setUserTemplate(bottom.target.value)}
         />
       </label>
       <br />
       <br />
 
-      <img src={`${templates}`} alt="Alt text" data-test-id="meme-image" />
+      <img src={templates} alt="Alt text" data-test-id="meme-image" />
     </div>
   );
 }
