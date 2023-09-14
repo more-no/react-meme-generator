9be61edc88c6e160fa2d7309e94c7e3b29f69773
Moreno Dore <moreno.dore@tuta.io>
2023-09-14 14:11:41 +0200

    Start writing fecth function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import './App.css';
-import logo from './logo.svg';
+import { useState } from 'react';
+
+// const url = 'https://api.memegen.link/images';
 
 export default function App() {
+  const [imageData, setImageData] = useState([]);
+
+  const useEffect =
+    (() => {
+      fetch('https://api.memegen.link/images')
+        .then((response) => response.json())
+        .then((data) => {
+          setImageData(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    },
+    []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Image Data</h1>
+      <ul>
+        {imageData.map((index) => (
+          <li key={index}>
+            {/* Render the properties of each object in the array */}
+            <p>TEMPLATE: {item.template}</p>
+            <p>URL: {item.url}</p>
+            {/* Add more properties as needed */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
