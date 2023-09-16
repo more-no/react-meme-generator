848330d8e00f71b3c078b16a8aee3a306e5c7172
Moreno Dore <moreno.dore@tuta.io>
2023-09-16 19:15:10 +0200

    Add styling

--- src/App.js
+++ src/App.js
@@ -23,51 +23,44 @@ export default function App() {
   }
 
   return (
-    <div className="wrapper">
-      <div className="sidebar">
-        <br />
-        <br />
-        <label className="label1">
-          Top text <br />
-          <input
-            value={userTopText}
-            onChange={(top) => setUserTopText(top.target.value)}
-          />
-        </label>
+    <div className="frame">
+      <div className="wrapper">
+        <div className="input">
+          <label className="label1">
+            Top text <br />
+            <input
+              value={userTopText}
+              onChange={(top) => setUserTopText(top.target.value)}
+            />
+          </label>
+          <br />
 
-        <br />
-        <br />
-        <label className="label2">
-          Bottom text <br />
-          <input
-            value={userBottomText}
-            onChange={(bottom) => setUserBottomText(bottom.target.value)}
-          />
-        </label>
+          <label className="label2">
+            Bottom text <br />
+            <input
+              value={userBottomText}
+              onChange={(bottom) => setUserBottomText(bottom.target.value)}
+            />
+          </label>
+          <br />
 
-        <br />
-        <br />
-        <label className="label3">
-          Meme template <br />
-          <input
-            value={userTemplate}
-            onChange={(bottom) => setUserTemplate(bottom.target.value)}
-          />
-        </label>
+          <label className="label3">
+            Meme template <br />
+            <input
+              value={userTemplate}
+              onChange={(bottom) => setUserTemplate(bottom.target.value)}
+            />
+          </label>
+          <br />
 
-        <br />
-        <br />
-        <button className="button" onClick={downloadImage}>
-          Download
-        </button>
-      </div>
+          <button className="button" onClick={downloadImage}>
+            Download
+          </button>
+        </div>
 
-      <div className="content">
-        <br />
-        <br />
-        <img src={templates} alt={userTemplate} data-test-id="meme-image" />
-        <br />
-        <br />
+        <div className="content">
+          <img src={templates} alt={userTemplate} data-test-id="meme-image" />
+        </div>
       </div>
     </div>
   );
